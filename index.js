2d9d23200f1b7c6121c6bdcacb62a580d91437a0
Erika Leão <leaoerikaleao@gmail.com>
2021-11-15 17:48:36 -0300

    public dirname refactor

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 3000
 
 const indexRouter = require('./routes/index')
 const tabuadaRouter = require('./routes/tabuada')
-app.use(express.static('public'))
+//app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // view engine
 app.set('views', path.join(__dirname, 'views'))
